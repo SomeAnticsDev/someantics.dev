43aa020ece77dbd7d515306f8c5acddcac825c8e
Ben Myers <ben.myers@okstate.edu>
2021-03-13 15:04:15 -0600

    Font support

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 module.exports = (eleventyConfig) => {
 	eleventyConfig.addCollection('streams', (collectionApi) => {
 		return collectionApi.getFilteredByGlob('./src/streams/*.md');
-	})
+	});
+
+	eleventyConfig.addPassthroughCopy('./src/css/');
 
 	return {
 		dir: {
