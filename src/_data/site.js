ebb5c6200bd4d32f614b339986c966a73d224779
Ben Myers <ben.myers@okstate.edu>
2021-05-03 22:00:28 -0500

    Fix thumbnail fonts

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,5 +1,6 @@
 module.exports = () => {
 	return process.env.ELEVENTY_ENV === 'production' ?
-		'https://someantics.dev' :
+		// 'https://someantics.dev' :
+		'https://someantics.netlify.app/css/fonts.css' :
 		'http://localhost:8080'
 }
