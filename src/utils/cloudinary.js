4d55c60dfc1d679b852351eb52f41d5ca3cf72f1
Ben Myers <ben.myers@okstate.edu>
2021-05-06 13:06:17 -0500

    Ashlae's upload and transcript

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -5,7 +5,7 @@
  */
 function avatar(profileName) {
 	let encodedProfileName = profileName.replace(/ /g, '%20');
-	return `https://res.cloudinary.com/bendmyers/image/upload/c_thumb,g_face,q_auto,f_auto,w_400,h_400/v1617494798/Some%20Antics%20Avatars/${encodedProfileName}.jpg`;
+	return `https://res.cloudinary.com/bendmyers/image/upload/c_thumb,g_face,q_auto,f_auto,w_400,h_400/v1617494798/Some%20Antics%20Avatars/${encodedProfileName}`;
 }
 
 module.exports = {
