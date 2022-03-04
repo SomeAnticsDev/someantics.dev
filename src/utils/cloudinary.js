eab74486013be68641f0168d0246fe84355ae25c
Ben Myers <ben.myers@okstate.edu>
2022-03-04 13:18:54 -0600

    Improve custom avatar use; update Dave's stream

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,9 +1,14 @@
 /**
  * Generates a Cloudinary-optimized image for the specified profile
  * @param {string} profileName Name of person whose avatar we're fetching
+ * @param {Object<string, Profile>} [profiles] all profiles
  * @returns {string} Optimized Cloudinary URL for the given avatar
  */
-function avatar(profileName) {
+function avatar(profileName, profiles = {}) {
+	if (profiles[profileName] && profiles[profileName].customAvatarUrl) {
+		return profiles[profileName].customAvatarUrl;
+	}
+
 	let encodedProfileName = profileName.replace(/ /g, '%20');
 	return `https://res.cloudinary.com/bendmyers/image/upload/c_thumb,g_face,q_auto,f_auto,w_400,h_400/v1617494798/Some%20Antics%20Avatars/${encodedProfileName}`;
 }
@@ -11,3 +16,8 @@ function avatar(profileName) {
 module.exports = {
 	avatar
 };
+
+/**
+ * @type {Object} Profile
+ * @property {string} [customAvatarUrl] manually provided Cloudinary link
+ */
